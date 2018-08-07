6836984141610b2da5508b44b5e1be325484a2b1
John Franklin Crenshaw <jfcrenshaw@gmail.com>
2018-08-07 11:48:05 +0200

    removed core distance from the data. changed some of the variable names in neural_network_data.py to be more readable

--- neural_network_data.py
+++ neural_network_data.py
@@ -9,27 +9,26 @@ def process_shower(shower):
     E_proton  = shower.Reconstruction.E_Proton
     E_iron    = shower.Reconstruction.E_Iron
     Zen       = shower.Reconstruction.zen
-    coredist  = shower.Reconstruction.CoreDist()
     Type      = shower.Primary.Type
     
     # for log(E_avg) > 16.5
-    Q400      = 0.
-    MuonPE400 = 0.
-    nMuon400  = 0.
+    Q400    = 0.
+    MuonVEM = 0.
+    nMuon   = 0.
 
     
     for i in range(len(shower.Signals.Tank)):
         if shower.Signals.LatDist[i] > 400 and shower.Signals.TotalPE[i] != 0:
             totalVEM = shower.Signals.TotalVEM[i]
             totalPE  = shower.Signals.TotalPE[i]
-            scale = totalVEM/totalPE
+            scale    = totalVEM/totalPE
             scaledPE = scale*shower.Signals.MuonPE[i]
-            nMuon400 += shower.Signals.nMuons[i]
-            MuonPE400 += scaledPE
+            nMuon   += shower.Signals.nMuons[i]
+            MuonVEM += scaledPE
             if totalVEM >= 0.6 and totalVEM <= 2.0:
                 Q400 += totalVEM
                 
-    NNdata400.append([E_proton,E_iron,Zen,coredist,Q400,MuonPE400,nMuon400,Type])
+    NNdata400.append([E_proton,E_iron,Zen,Q400,MuonVEM,nMuon,Type])
 
 
 protondata = np.load('./data/proton_showers.npy')
