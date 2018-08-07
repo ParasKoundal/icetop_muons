4a0fd08acb79f0253e78d3d2c3c3394ac634bee0
John Franklin Crenshaw <jfcrenshaw@gmail.com>
2018-08-07 15:38:24 +0200

    Added original NN data back in alongside the averaged data. Also changed the way the functions are written to handle the data

--- neural_network_data.py
+++ neural_network_data.py
@@ -2,40 +2,44 @@
 
 import numpy as np
 
-NNdata400  = []
 
 # function that collects the data wanted for use in the neural network
-def process_shower(shower):
-    Run       = shower.Run
-    E_proton  = shower.Reconstruction.E_Proton
-    E_iron    = shower.Reconstruction.E_Iron
-    Zen       = shower.Reconstruction.zen
-    Type      = shower.Primary.Type
-    
+def process_showers(showers):
+    List = []
+    for shower in showers:
+        Run       = shower.Run
+        E_proton  = shower.Reconstruction.E_Proton
+        E_iron    = shower.Reconstruction.E_Iron
+        Zen       = shower.Reconstruction.zen
+        Type      = shower.Primary.Type
+        
 
-    Q400    = 0.
-    MuonVEM = 0.
-    nMuon   = 0.
+        Q400    = 0.
+        MuonVEM = 0.
+        nMuons   = 0.
 
-    
-    for i in range(len(shower.Signals.Tank)):
-        if shower.Signals.LatDist[i] > 400 and shower.Signals.TotalPE[i] != 0:
-            totalVEM = shower.Signals.TotalVEM[i]
-            totalPE  = shower.Signals.TotalPE[i]
-            scale    = totalVEM/totalPE
-            scaledPE = scale*shower.Signals.MuonPE[i]
-            nMuon   += shower.Signals.nMuons[i]
-            MuonVEM += scaledPE
-            if totalVEM >= 0.6 and totalVEM <= 2.0:
-                Q400 += totalVEM
+        
+        for i in range(len(shower.Signals.Tank)):
+            if shower.Signals.LatDist[i] > 400 and shower.Signals.TotalPE[i] != 0:
+                totalVEM = shower.Signals.TotalVEM[i]
+                totalPE  = shower.Signals.TotalPE[i]
+                scale    = totalVEM/totalPE
+                scaledPE = scale*shower.Signals.MuonPE[i]
+                nMuons   += shower.Signals.nMuons[i]
+                MuonVEM += scaledPE
+                if totalVEM >= 0.6 and totalVEM <= 2.0:
+                    Q400 += totalVEM
 
-    NNdata400_.append([Run,E_proton,E_iron,Zen,Q400,MuonVEM,nMuon,Type])
+        List.append([Run,E_proton,E_iron,Zen,Q400,MuonVEM,nMuons,Type])
+    return List
 
 # function that avarages the NN data for each run
-def avg_runs(NNdata400_,Type):
+def avg_runs(List,Type):
+    
+    avgList = []
     
     run_list = set()
-    for shower in NNdata400_:
+    for shower in List:
         run_list.add(shower[0])
     
     for run in run_list:
@@ -45,8 +49,8 @@ def avg_runs(NNdata400_,Type):
         Zen_tot      = 0.
         Q400_tot     = 0.
         MuonVEM_tot  = 0.
-        nMuon_tot    = 0.
-        for shower in NNdata400_:
+        nMuons_tot    = 0.
+        for shower in List:
             if shower[0] == run:
                 nShowers     += 1.
                 E_proton_tot += shower[1]
@@ -54,37 +58,41 @@ def avg_runs(NNdata400_,Type):
                 Zen_tot      += shower[3]
                 Q400_tot     += shower[4]
                 MuonVEM_tot  += shower[5]
-                nMuon_tot    += shower[6]
+                nMuons_tot    += shower[6]
                 
         E_proton_avg = E_proton_tot/nShowers
         E_iron_avg   = E_iron_tot/nShowers
         Zen_avg      = Zen_tot/nShowers
         Q400_avg     = Q400_tot/nShowers
         MuonVEM_avg  = MuonVEM_tot/nShowers
-        nMuon_avg    = nMuon_tot/nShowers
-        NNdata400.append([E_proton_avg,E_iron_avg,Zen_avg,Q400_avg,MuonVEM_avg,nMuon_avg,Type])
+        nMuons_avg    = nMuons_tot/nShowers
+        avgList.append([E_proton_avg,E_iron_avg,Zen_avg,Q400_avg,MuonVEM_avg,nMuons_avg,Type])
+    
+    return avgList
 
 
-
-
-
-
-
-NNdata400_ = []
+# first the proton data
 protondata = np.load('./data/proton_showers.npy')
-for shower in protondata:
-    process_shower(shower)
-del protondata
-avg_runs(NNdata400_,"proton")
+NNdata_proton = process_showers(protondata) # original data
+del protondata # to save memory
+NNdata_proton_avg = avg_runs(NNdata_proton,"PPlus") # average by run number
 
-NNdata400_ = []
+# now the iron data
 irondata = np.load('./data/iron_showers.npy')
-for shower in irondata:
-    process_shower(shower)
-del irondata
-avg_runs(NNdata400_,"iron")
+NNdata_iron = process_showers(irondata) # original data
+del irondata # to save memory
+NNdata_iron_avg = avg_runs(NNdata_iron,'Fe56Nucleus') # average by run number
 
+# combine proton and iron
+NNdata     = np.concatenate((NNdata_proton,NNdata_iron))
+NNdata_avg = np.concatenate((NNdata_proton_avg,NNdata_iron_avg))
+
+
+
+# save the data
 
 print 'saving ./data/NN_data_400m.npy'
-np.save('./data/NN_data_400m.npy',NNdata400)
+np.save('./data/NN_data_400m.npy',NNdata)
 
+print 'saving ./data/NN_data_400m_avg.npy'
+np.save('./data/NN_data_400m_avg.npy',NNdata_avg)
